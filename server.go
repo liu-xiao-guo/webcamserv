4014fc8b6443b65fd8c08712b26cbb59be7926e0
XiaoGuo, Liu <liu_xiao_guo@yahoo.com>
2016-09-21 13:56:48 +0800

    ddddd

--- server.go
+++ server.go
@@ -30,7 +30,7 @@ func main() {
 func getPicture(w http.ResponseWriter, r *http.Request) {
 	log.Println("entering getPicture")
 	// cmd := exec.Command("fswebcam", "--png 5 -")
-	cmd := exec.Command("fswebcam", "--jpeg 10",  "-r 320x240", "-")
+	cmd := exec.Command("fswebcam", "--jpeg", "10",  "-r", "320x240", "-")
 	
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
